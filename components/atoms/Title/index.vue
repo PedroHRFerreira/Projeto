33c0f5720a153abd2bb9e812740ca98b8ab28613
Pedro Henrique <prdrinho13@gmail.com>
2024-05-20 15:34:44 -0300

    feat: :sparkles: Criação de componentes/ambiente

---
+++ components/atoms/Title/index.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AtomsTitle",
+  props: {
+    sizeTitle: {
+      type: String,
+      default: "large",
+      validator: (value: string) => {
+        return [
+          "extra-large",
+          "large",
+          "medium",
+          "small",
+          "extra-small",
+        ].includes(value);
+      },
+    },
+    typeTitle: {
+      type: String,
+      default: "dark",
+      validator: (value: string) => {
+        return ["default", "primary", "dark"].includes(value);
+      },
+    },
+    text: {
+      type: String,
+      default: "Title",
+    },
+  },
+  emits: ["onclick"],
+  setup(props, { emit }) {
+    const titleOnClick = () => {
+      console.log("click in title");
+      emit("onclick");
+    };
+
+    return {
+      titleOnClick,
+    };
+  },
+});
+</script>
+
+<template>
+  <h1 class="title-text" :class="[sizeTitle, typeTitle]" @click="titleOnClick">
+    {{ text }}
+  </h1>
+</template>
+
+<style scoped lang="scss">
+@import "styles.module.scss";
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+<template>
+  <AtomsTitle />
+</template>
