ccd6825a448f66ce33711b13ac6e87d92a717e11
Pedro Henrique <prdrinho13@gmail.com>
2024-05-22 17:22:15 -0300

    feat: :sparkles: Criação do AtomsSwitch

---
+++ components/atoms/Switch/index.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "AtomsSwitch",
+  emits: ["onSwitch"],
+  props: {
+    backgroundOff: {
+      type: String,
+      default: "red",
+    },
+    backgroundOn: {
+      type: String,
+      default: "green",
+    },
+  },
+  setup(props) {
+    const isOn = ref(false);
+
+    const toggleSwitch = () => {
+      isOn.value = !isOn.value;
+    };
+
+    const cssVar = computed(() => {
+      return {
+        "--background-off": props.backgroundOff,
+        "--background-on": props.backgroundOn,
+      };
+    });
+
+    return {
+      isOn,
+      toggleSwitch,
+      cssVar,
+    };
+  },
+});
+</script>
+<template>
+  <section>
+    <div class="border-circle" :class="{ 'border-circle--on': isOn }">
+      <div
+        :style="cssVar"
+        class="border-circle__before-after"
+        :class="{ 'border-circle__on': isOn }"
+        @click="toggleSwitch"
+      ></div>
+    </div>
+  </section>
+</template>
+<style scoped lang="scss">
+@import "styles.module.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,5 @@ export default defineComponent({
 });
 </script>
 <template>
-  <AtomsImageDisplay />
+  <AtomsSwitch />
 </template>
