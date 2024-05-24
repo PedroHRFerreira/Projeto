82bc45388f783074e44b00fc79cdbd8761249a28
Pedro Henrique <prdrinho13@gmail.com>
2024-05-24 15:16:39 -0300

    feat: :sparkles: Criação do MoleculesIconProfile

---
+++ components/molecules/IconProfile/index.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+export default defineComponent({
+  name: "MoleculesIconProfile",
+  setup() {
+    const items = computed(() => {
+      return [
+        {
+          icon: "github-fill-svgrepo-com",
+          text: "Github",
+        },
+        {
+          icon: "github-fill-svgrepo-com",
+          text: "Github",
+        },
+        {
+          icon: "github-fill-svgrepo-com",
+          text: "Github",
+        },
+      ];
+    });
+
+    return {
+      items,
+    };
+  },
+});
+</script>
+<template>
+  <article class="section">
+    <div v-for="(item, index) in items" :key="index" class="icon-text">
+      <AtomsIconSVG :name="item.icon" width="34px" height="34px" />
+      <div>
+        <AtomsParagraphTitle :text="item.text" size="extra-small" />
+      </div>
+    </div>
+  </article>
+</template>
+<style scoped lang="scss">
+@import "styles.module.scss";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,5 @@ export default defineComponent({
 });
 </script>
 <template>
-  <AtomsImageDisplay />
+  <MoleculesIconProfile />
 </template>
