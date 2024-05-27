34c8ebed32d2052218af8c790b7292c0f1d4e979
Pedro Henrique <prdrinho13@gmail.com>
2024-05-27 15:30:51 -0300

    feat: :fire: remove

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,24 +17,15 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["~/assets/font/main.css"],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
-  ],
+  buildModules: ["@nuxt/typescript-build"],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["@nuxt/image"],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 };
