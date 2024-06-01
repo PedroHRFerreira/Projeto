43775b766136c1e46309b0077d041acd86daf3d0
Pedro Henrique <prdrinho13@gmail.com>
2024-06-01 15:20:33 -0300

    feat: :sparkles: Criação do AtomsTag

---
+++ components/atoms/Tag/index.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+export default defineComponent({
+  name: "AtomsTag",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "outline",
+      validator: (value: string) => {
+        return ["outline", "filter"].includes(value);
+      },
+    },
+    state: {
+      type: String,
+      default: "default",
+      validator: (value: string) => {
+        return [
+          "default",
+          "recovered",
+          "open",
+          "negotiation",
+          "payment_pending",
+          "closed",
+          "heated",
+          "active",
+          "inactive",
+        ].includes(value);
+      },
+    },
+  },
+  emits: ["onClose"],
+  setup(props, { emit }) {
+    const closeTag = () => {
+      emit("onClose");
+      console.log("test", emit);
+    };
+
+    const isFilter = computed(() => {
+      return props.type === "filter";
+    });
+
+    return {
+      closeTag,
+      isFilter,
+    };
+  },
+});
+</script>
+<template>
+  <article :class="['tag', type, state]">
+    {{ text }}
+    <AtomsIconSVG
+      v-if="isFilter"
+      name="close"
+      width="14px"
+      filled
+      heigth="14px"
+      @click="closeTag"
+    />
+  </article>
+</template>
+<style scoped lang="scss">
+@import "styles.module.scss";
+</style>
