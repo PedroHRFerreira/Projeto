3787ad73db8e22d8b8604ac96aafa5a943f49895
Pedro Henrique <prdrinho13@gmail.com>
2024-06-04 15:08:14 -0300

    feat: :sparkles: Criação da moleculesButton

---
+++ components/molecules/Button/index.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "moleculesButton",
+  props: {
+    size: {
+      type: String,
+      default: "primary",
+      validator: (value: string) => {
+        return ["small", "primary", "medium", "big"].includes(value);
+      },
+    },
+    type: {
+      type: String,
+      default: "cancel",
+      validator: (value: string) => {
+        return ["dafult", "cancel"].includes(value);
+      },
+    },
+    sizeText: {
+      type: String,
+      default: "extra-small",
+    },
+    text: {
+      type: String,
+      default: "Button test",
+    },
+  },
+  emits: ["Onclick", "OffClick"],
+  setup(props, { emit }) {
+    const ButtonClick = () => {
+      emit("Onclick");
+    };
+
+    const cancel = () => {
+      emit("OffClick");
+    };
+    return {
+      ButtonClick,
+      cancel,
+    };
+  },
+});
+</script>
+<template>
+  <button
+    class="recreational-button"
+    :class="[size, type]"
+    @click="ButtonClick"
+    @close="cancel"
+  >
+    <AtomsParagraphTitle :size="sizeText" :text="text" />
+  </button>
+</template>
+<style scoped lang="scss">
+@import "styles.module.scss";
+</style>
