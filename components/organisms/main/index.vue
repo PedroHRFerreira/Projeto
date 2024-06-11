98ecc9217d8a3101afd5c614d8842edd47aa2f7a
Pedro Henrique <prdrinho13@gmail.com>
2024-06-11 17:17:07 -0300

    fix: :bug: .

--- components/organisms/main/index.vue
+++ components/organisms/main/index.vue
@@ -1,26 +1,48 @@
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { computed, defineComponent, onMounted } from "vue";
 import { useMainStore } from "~/store/main/useMainStore";
 
 export default defineComponent({
   name: "OrganismsMain",
+  props: {},
   setup() {
     const store = useMainStore();
 
-    const profileData = store.mainProfile;
+    const profileData = computed(() => store.mainProfile);
 
     onMounted(() => {
       store.fetchMain();
     });
 
+    const items = computed(() => {
+      if (!profileData.value || !profileData.value.personalInformation) {
+        return [];
+      }
+      return profileData.value.personalInformation.map((item: any) => ({
+        title: item.information,
+        text: item.history,
+      }));
+    });
+
     return {
       profileData,
+      items,
     };
   },
 });
 </script>
 
-<template></template>
+<template>
+  <article>
+    <div class="main">
+      <div class="main-section" v-for="(item, index) in items" :key="index">
+        <AtomsParagraphTitle :text="profileData.profile" size="extra-small" />
+        <AtomsParagraphTitle :text="item.title" size="extra-small" />
+        <AtomsParagraphTitle :text="item.text" size="extra-small" />
+      </div>
+    </div>
+  </article>
+</template>
 
 <style scoped lang="scss">
 @import "styles.module.scss";
--- store/main/useMainStore.ts
+++ store/main/useMainStore.ts
@@ -23,8 +23,17 @@ export const useMainStore = defineStore("useMainStore", {
           profile: 1,
           personalInformation: [
             {
-              information: "teste de informações",
-              history: "teste de história",
+              information: "What is Lorem Ipsum?",
+              history:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            },
+            {
+              information: "teste de informações2",
+              history: "teste de história2",
+            },
+            {
+              information: "teste de informações2",
+              history: "teste de história2",
             },
           ],
         } as Imain,
