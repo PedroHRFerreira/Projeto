f5cf0e4233eef8e05ae1f886a876136452ae0565
Pedro Henrique <prdrinho13@gmail.com>
2024-06-30 16:26:54 -0300

    Criação do MoleculesSkills

---
+++ components/molecules/Skills/index.vue
@@ -0,0 +1,70 @@
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+export default defineComponent({
+  name: "MoleculesSkills",
+  emits: ["onClick"],
+  setup() {
+    const items = computed(() => {
+      return [
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+        {
+          icon: "photo",
+          color: "#fff",
+        },
+      ];
+    });
+
+    return {
+      items,
+    };
+  },
+});
+</script>
+<template>
+  <article class="skills-content">
+    <div
+      v-for="(item, index) in items"
+      :key="index"
+      class="skills-content__main"
+      @click="$emit('onClick')"
+    >
+      <AtomsIconSVG
+        class="icon"
+        :name="item.icon"
+        :current-color="item.color"
+        width="32px"
+        filled
+      />
+    </div>
+  </article>
+</template>
+<style scoped lang="scss">
+@import "styles.module.scss";
+</style>
